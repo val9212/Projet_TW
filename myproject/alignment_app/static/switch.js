e616f9702db8e33fa2a7c742273427aeb3f2d4f5
val9212 <val92.hottlet@gmail.com>
2024-12-06 00:34:33 +0100

    Refactor navbar layout

--- myproject/alignment_app/static/switch.js
+++ myproject/alignment_app/static/switch.js
@@ -2,30 +2,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggleCheckbox = document.getElementById('toggle-style');
     const themeLink = document.getElementById('theme-style');
 
-    // Récupérer le thème sauvegardé
     let savedTheme = localStorage.getItem('theme');
     if (!savedTheme) {
-        savedTheme = '/static/style/style.css';  // Par défaut, mode clair
+        savedTheme = '/static/style/style.css';
     }
 
-    // Appliquer le thème sauvegardé
     themeLink.setAttribute('href', savedTheme);
 
-    // Mettre à jour l'état du switch en fonction du thème
     if (savedTheme === '/static/style/style_sombre.css') {
         themeToggleCheckbox.checked = true;
     } else {
         themeToggleCheckbox.checked = false;
     }
 
-    // Ajouter l'événement change pour le switch
     themeToggleCheckbox.addEventListener('change', () => {
         const newTheme = themeToggleCheckbox.checked
             ? '/static/style/style_sombre.css'
             : '/static/style/style.css';
 
         themeLink.setAttribute('href', newTheme);
-        // Sauvegarder le nouveau thème dans localStorage
         localStorage.setItem('theme', newTheme);
     });
 });
