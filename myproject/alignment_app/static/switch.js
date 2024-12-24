2d792869ee8a4842801a74898e68ab13c8855830
val9212 <val92.hottlet@gmail.com>
2024-12-24 07:26:28 +0100

    Refactor static file paths to use Django's static template tag.

--- myproject/alignment_app/static/switch.js
+++ myproject/alignment_app/static/switch.js
@@ -2,23 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggleCheckbox = document.getElementById('toggle-style');
     const themeLink = document.getElementById('theme-style');
 
+    // Assume `themeCSS` is defined as global in the HTML
     let savedTheme = localStorage.getItem('theme');
     if (!savedTheme) {
-        savedTheme = '/static/style/style.css';
+        savedTheme = themeCSS.light;
     }
 
     themeLink.setAttribute('href', savedTheme);
 
-    if (savedTheme === '/static/style/style_sombre.css') {
-        themeToggleCheckbox.checked = true;
-    } else {
-        themeToggleCheckbox.checked = false;
-    }
+    themeToggleCheckbox.checked = (savedTheme === themeCSS.dark);
 
     themeToggleCheckbox.addEventListener('change', () => {
-        const newTheme = themeToggleCheckbox.checked
-            ? '/static/style/style_sombre.css'
-            : '/static/style/style.css';
+        const newTheme = themeToggleCheckbox.checked ? themeCSS.dark : themeCSS.light;
 
         themeLink.setAttribute('href', newTheme);
         localStorage.setItem('theme', newTheme);
